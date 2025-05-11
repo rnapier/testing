6d18ecaa1c5ff773211ada3b4ef73a0f62c83c90
Rob Napier <ornapier@audible.com>
2025-05-10 21:06:53 -0400

    Refactor out remove

--- Keychain/Sources/Keychain/Keychain.swift
+++ Keychain/Sources/Keychain/Keychain.swift
@@ -13,44 +13,9 @@ public actor Keychain {
 
   private var cache: [String: Data] = [:]
 
-  public func reset() throws {
-    cache = [:]
-
-    var query = keychainDictionary(for: nil)
-#if os(macOS)
-    query[kSecMatchLimit] = kSecMatchLimitAll
-#endif
-
-    let status = SecItemDelete(query as CFDictionary)
-
-    guard status == errSecSuccess || status == errSecItemNotFound else {
-      throw Error.keychain(status)
-    }
-  }
-
-  public func string(for key: String) throws -> String? {
-    guard let data = try data(for: key) else { return nil }
-    return String(data: data, encoding: .utf8)
-  }
-
-  public func set(string: String?, for key: String) throws {
-    try set(data: string.map { Data($0.utf8) }, for: key)
-  }
-
-  public func value(for key: String) throws -> Any? {
-    guard let data = try data(for: key) else { return nil }
-    return try JSONSerialization.jsonObject(with: data, options: [.fragmentsAllowed])
-  }
-
-  public func set(value: Any?, for key: String) throws {
-    guard let value else {
-      try set(data: nil, for: key)
-      return
-    }
-
-    try set(data: JSONSerialization.data(withJSONObject: value, options: [.fragmentsAllowed]), for: key)
-  }
-
+  //
+  // MARK: Data is written directly
+  //
   public func data(for key: String) throws -> Data? {
     if let data = cache[key] {
       return data
@@ -79,48 +44,99 @@ public actor Keychain {
   }
 
   public func set(data: Data?, for key: String) throws {
+    guard let data else {
+      try removeData(for: key)
+      return
+    }
+
     cache[key] = data
 
     var params = keychainDictionary(for: key)
-    let query: () -> OSStatus = {
-      // if nil, then delete instead of update
-      if data == nil {
-        return SecItemDelete(params as CFDictionary)
-      } else {
-        // first lets attempt to update the entry
-        return SecItemUpdate(
-          params as CFDictionary,
-          [
-            kSecValueData: data
-          ] as CFDictionary
-        )
-      }
-    }
 
-    var status = query()
-    if status == errSecItemNotFound && data != nil {
+    // Attempt to update the entry
+    var status = SecItemUpdate(
+      params as CFDictionary,
+      [
+        kSecValueData: data
+      ] as CFDictionary
+    )
+
+    // If it doesn't exist, try adding it
+    if status == errSecItemNotFound  {
       params[kSecAttrAccessible] = kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly
       params[kSecValueData] = data
       status = SecItemAdd(params as CFDictionary, nil)
     }
+
     guard status == errSecSuccess else {
       throw Error.keychain(status)
     }
   }
 
-  private func keychainDictionary(for id: String?) -> [CFString: Any] {
-    var query: [CFString: Any] = [
-      kSecAttrGeneric: Data(keychainID.utf8),
-      kSecClass: kSecClassGenericPassword,
-    ]
-    if let id {
-      query[kSecAttrService] = id
+  public func removeData(for key: String) throws {
+    cache[key] = nil
+
+    let params = keychainDictionary(for: key)
+    let status =  SecItemDelete(params as CFDictionary)
+
+    guard status == errSecSuccess || status == errSecItemNotFound else {
+      throw Error.keychain(status)
     }
-    return query
+  }
+
+  //
+  // MARK: Strings are encoded as UTF-8
+  //
+  public func string(for key: String) throws -> String? {
+    guard let data = try data(for: key) else { return nil }
+    return String(data: data, encoding: .utf8)
+  }
+
+  public func set(string: String?, for key: String) throws {
+    try set(data: string.map { Data($0.utf8) }, for: key)
+  }
+
+  //
+  // MARK: All other values are encoded with JSONSerialization
+  //
+  // Note that the caller must ensure that this is safe.
+  //
+  public func value(for key: String) throws -> Any? {
+    guard let data = try data(for: key) else { return nil }
+    return try JSONSerialization.jsonObject(with: data, options: [.fragmentsAllowed])
+  }
+
+  public func set(value: Any?, for key: String) throws {
+    let data = try value.map {
+      try JSONSerialization.data(withJSONObject: $0, options: [.fragmentsAllowed])
+    }
+
+    try set(data: data, for: key)
+  }
+
+  //
+  // MARK: Reset clears all values
+  //
+  public func reset() throws {
+    cache = [:]
+
+    var query = keychainDictionary(for: nil)
+#if os(macOS)
+    query[kSecMatchLimit] = kSecMatchLimitAll
+#endif
+
+    let status = SecItemDelete(query as CFDictionary)
+
+    guard status == errSecSuccess || status == errSecItemNotFound else {
+      throw Error.keychain(status)
+    }
   }
 }
 
-// Helpers
+//
+// MARK: - Public Helpers for common types
+//
+// Note that these do not distinguish type mismatch from missing values.
 extension Keychain {
   public func bool(for key: String) throws -> Bool? {
     try value(for: key) as? Bool
@@ -136,5 +152,21 @@ extension Keychain {
 
   public func set(int: Int?, for key: String) throws {
     try set(value: int, for: key)
+  }
+}
+
+//
+// MARK: - Private Helpers
+//
+private extension Keychain {
+  private func keychainDictionary(for id: String?) -> [CFString: Any] {
+    var query: [CFString: Any] = [
+      kSecAttrGeneric: Data(keychainID.utf8),
+      kSecClass: kSecClassGenericPassword,
+    ]
+    if let id {
+      query[kSecAttrService] = id
+    }
+    return query
   }
 }
